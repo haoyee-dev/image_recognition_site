8451a259f7dd1045c58771d12eee121306c22bfc
haoyeec <haoyee.dev@gmail.com>
2017-06-26 15:43:36 +1000

    Changed to WebRTC implementation

--- main.js
+++ main.js
@@ -1,27 +1,159 @@
-$(document).ready(function() {
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    var video = document.querySelector('video');
-    var stream;
-    navigator.mediaDevices.getUserMedia({video:true}).then( function(localMediaStream) {
-        stream = localMediaStream;
-        video.src = window.URL.createObjectURL(localMediaStream);
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+var videoElement = document.querySelector('video');
+//var audioInputSelect = document.querySelector('select#audioSource');
+//var audioOutputSelect = document.querySelector('select#audioOutput');
+var videoSelect = document.querySelector('select#videoSource');
+//var selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+var selectors = [videoSelect];
+
+function gotDevices(deviceInfos) {
+
+    // Handles being called several times to update labels. Preserve values.
+    var values = selectors.map(function (select) {
+        return select.value;
     });
 
+    selectors.forEach(function (select) {
+        while (select.firstChild) {
+            select.removeChild(select.firstChild);
+        }
+    });
+
+    for (var i = 0; i !== deviceInfos.length; ++i) {
+        var deviceInfo = deviceInfos[i];
+        var option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === 'audioinput') {
+           // option.text = deviceInfo.label ||
+             //   'microphone ' + (audioInputSelect.length + 1);
+            //audioInputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'audiooutput') {
+            //option.text = deviceInfo.label || 'speaker ' + (audioOutputSelect.length + 1);
+            //audioOutputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+            videoSelect.appendChild(option);
+        } else {
+            console.log('Some other kind of source/device: ', deviceInfo);
+        }
+    }
+
+    selectors.forEach(function (select, selectorIndex) {
+        if (Array.prototype.slice.call(select.childNodes).some(function (n) {
+                return n.value === values[selectorIndex];
+            })) {
+            select.value = values[selectorIndex];
+        }
+    });
+}
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+    if (typeof element.sinkId !== 'undefined') {
+        element.setSinkId(sinkId)
+            .then(function () {
+                console.log('Success, audio output device attached: ' + sinkId);
+            })
+            .catch(function (error) {
+                var errorMessage = error;
+                if (error.name === 'SecurityError') {
+                    errorMessage = 'You need to use HTTPS for selecting audio output ' + 'device: ' + error;
+                }
+                console.error(errorMessage);
+
+                // Jump back to first output device in the list as it's the default.
+                audioOutputSelect.selectedIndex = 0;
+            });
+    } else {
+        console.warn('Browser does not support output device selection.');
+    }
+}
+/*
+function changeAudioDestination() {
+    var audioDestination = audioOutputSelect.value;
+    attachSinkId(videoElement, audioDestination);
+}
+*/
+function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoElement.srcObject = stream;
+
+    // Refresh button list in case labels have become available
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function start() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(function (track) {
+            track.stop();
+        });
+    }
+
+//    var audioSource = audioInputSelect.value;
+    var videoSource = videoSelect.value;
+    var constraints = {
+/*
+        audio: {
+            deviceId: audioSource ? {
+                exact: audioSource
+            } : undefined
+        },
+*/
+        video: {
+            deviceId: videoSource ? {
+                exact: videoSource
+            } : undefined
+        }
+
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).then(gotDevices).catch(handleError);
+}
+
+/*
+audioInputSelect.onchange = start;
+audioOutputSelect.onchange = changeAudioDestination;
+*/
+videoSelect.onchange = start;
+
+start();
+submitImage();
+
+function handleError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+function submitImage() {
     var canvas = document.getElementById('canvas');
     var form = document.getElementById('form');
     var context = canvas.getContext('2d');
     var snap = document.getElementById('snap');
     var submit = document.getElementById('submit');
 
-    snap.addEventListener('click', function() {
+    snap.addEventListener('click', function () {
         context.drawImage(video, 0, 0, 320, 240);
-        stream.getVideoTracks().forEach(function(s) { s.stop()});
+        stream.getVideoTracks().forEach(function (s) {
+            s.stop()
+        });
         form.style.display = 'block';
         video.style.display = 'none';
         snap.style.display = 'none';
     })
 
-    submit.addEventListener('click', function(e) {
+    submit.addEventListener('click', function (e) {
         e.preventDefault();
         console.log(canvas.toDataURL());
 
@@ -32,13 +164,59 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             contentType: "text/plain",
             method: "POST",
             processData: false,
-            success:  function(data) {
+            success: function (data) {
                 $("#result").html(data);
             }
         })
     })
-}
-else {
-    console.log("no media");
-}
+} 
+
+/*
+$(document).ready(function () {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        var video = document.querySelector('video');
+        var stream;
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(function (localMediaStream) {
+            stream = localMediaStream;
+            video.src = window.URL.createObjectURL(localMediaStream);
+        });
+
+        var canvas = document.getElementById('canvas');
+        var form = document.getElementById('form');
+        var context = canvas.getContext('2d');
+        var snap = document.getElementById('snap');
+        var submit = document.getElementById('submit');
+
+        snap.addEventListener('click', function () {
+            context.drawImage(video, 0, 0, 320, 240);
+            stream.getVideoTracks().forEach(function (s) {
+                s.stop()
+            });
+            form.style.display = 'block';
+            video.style.display = 'none';
+            snap.style.display = 'none';
+        })
+
+        submit.addEventListener('click', function (e) {
+            e.preventDefault();
+            console.log(canvas.toDataURL());
+
+            $.ajax({
+                url: "https://us-central1-image-recognition-171007.cloudfunctions.net/translateImage",
+                data: canvas.toDataURL(),
+                dataType: "text",
+                contentType: "text/plain",
+                method: "POST",
+                processData: false,
+                success: function (data) {
+                    $("#result").html(data);
+                }
+            })
+        })
+    } else {
+        console.log("no media");
+    }
 });
+*/
